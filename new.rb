daed0acbe6508a516905f7f4976a08390834416e
kosuke-byte <119716029+kosuke->
2022-12-15 02:54:27 +0000

    [FINISH]2-9

--- new.rb
+++ new.rb
@@ -1,4 +1,10 @@
 class Car
+  
+  def move(direction, distance)
+    self.turn(direction)
+    self.run(distance)
+  end
+  
   def turn(direction)
     puts "車が#{direction}に曲がります。"
   end
@@ -9,8 +15,20 @@ class Car
 end
 
 puts "入力してください"
+
+car =Car.new
+car.move(gets, gets)
+
 car = Car.new
 car.run(gets.to_i)
 
 car = Car.new
 car.turn(gets)
+
+class Car
+  def self.run(distance)
+    puts "車で#{distance}キロ走ります。"
+  end
+end
+
+Car.run(gets)
