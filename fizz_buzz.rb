d3906316b788c0a3ae0a2e4c7a81b5277716e01d
kosuke-byte <119716029+kosuke->
2022-12-15 00:48:02 +0000

    [修正]

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,21 +1,22 @@
 def fizz_buzz(input)
-  
-  if input % 3 == 0 
+
+  # 最初にFizzBuzzを持ってこないと全部Fizzになってしまう。
+
+  if input % 3 == 0 && input % 5 == 0
+    puts "FizzBuzz"
+
+  elsif input % 3 == 0
     puts "Fizz"
-    
-  elsif input % 3 == 0 && input % 5 == 0
-     puts "FizzBuzz"
-    
+
   else input % 5 == 0
     puts "Buzz"
-    
   end
 end
 
 puts "数字を入力してください。"
 
  input = gets.to_i
- 
+
  puts "結果は..."
- 
+
  puts fizz_buzz(input)
